10f41b5fb536d71e92b077fc8e9b8f50785340d0
Fatih Kabay <mfthkabay@gmail.com>
2022-09-12 22:39:35 +0300

    import routes route link and add home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="container">
-<h1>Hello World</h1>      
+      <h1>Hello World</h1>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route></Route>
+      </Routes>
     </div>
   );
+}
+
+function Home () {
+  return (
+    <h1>Welcome Home Page</h1>
+  )
 }
 
 export default App;

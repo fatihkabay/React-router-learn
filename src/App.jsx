cd8480435a56583884e1a5a96a1238389729e6a7
Fatih Kabay <mfthkabay@gmail.com>
2022-09-12 22:58:24 +0300

    use link

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,28 @@ function App() {
     <div className="container">
       <h1>Hello World</h1>
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Europa" element={<Europa />}></Route>
         <Route></Route>
       </Routes>
     </div>
   );
 }
 
-function Home () {
-  return (
-    <h1>Welcome Home Page</h1>
+function Home() {
+  return ( 
+     <h1>Welcome Home Page</h1>
   )
+
+}
+
+function Europa() {
+  return (
+    <div>
+      <h1>Welcome Europa Page</h1>
+      <Link to="/">Home</Link>
+    </div>
+  );
 }
 
 export default App;
